c594e7d92fb1cefa86d1065c889984331bc2bb65
amirsafaricg <a.safari1380@gmail.com>
2024-07-20 15:40:58 +0330

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="text-red-400">This is made red by tailwind css</div>
 </template>
 
 <script>
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/App.vue"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
