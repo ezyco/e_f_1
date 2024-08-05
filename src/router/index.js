3fce3f106c9bd61a86fa00bc7fc73707ab0351e2
Amir Safari <77434000+amirsafaricg@users.noreply.github.com>
2024-08-05 22:37:53 +0330

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,41 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import MenuPage from '@/components/MenuPage.vue';
-import ServerPage from '@/components/ServerPage.vue';
-import EditMenuPage from '@/components/EditMenuPage.vue';
-import SettingsPage from '@/components/SettingsPage.vue';
-import LoginPage from '@/components/LoginPage.vue';
-import ProfilePage from '@/components/ProfilePage.vue';
 import NotFoundPage from '@/components/NotFoundPage.vue'
 const routes = [
   {
-    path: '/',
+    path: '/menu/toptime',
     name: 'MenuPage',
     component: MenuPage
-  },
-  {
-    path: '/server',
-    name: 'ServerPage',
-    component: ServerPage
-  },
-  {
-    path: '/edit',
-    name: 'EditMenuPage',
-    component: EditMenuPage
-  },
-  {
-    path: '/settings',
-    name: 'SettingsPage',
-    component: SettingsPage
-  },
-  {
-    path: '/login',
-    name: 'LoginPage',
-    component: LoginPage,
-  },
-  {
-    path: '/profile',
-    name: 'ProfilePage',
-    component: ProfilePage,
   },
   {
     path: '/:pathMatch(.*)*',
